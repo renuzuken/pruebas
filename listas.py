7b968ddf21159ae19749460876c9d5a7c917f29e
renuzuken <renuzuken@gmail.com>
2023-05-05 17:09:20 -0400

    es

---
+++ listas.py
@@ -0,0 +1,13 @@
+mi_lista=[5,4,2,7,4]
+
+mi_lista.sort(reverse=True)
+
+#Agrega al ultimo de la lista e imprime el ultimo de la lista
+mi_lista.append(57)
+print("Ultimo ",mi_lista.pop())
+
+print(mi_lista)
+
+listupla=tuple(mi_lista)
+
+print(4 in mi_lista)
